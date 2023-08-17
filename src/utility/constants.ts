b885cde9d063a82c677cce843158082ddb30a438
Efen Stakes <efenstakes101@gmail.com>
2023-08-17 18:24:28 +0300

    add common constants used in the app

---
+++ src/utility/constants.ts
@@ -0,0 +1,7 @@
+
+
+export const SERVER_URL = ""
+
+export const PROFILE_ACCESS_TOKEN = "auto/spot/access-token"
+export const PROFILE_REFRESH_TOKEN = "auto/spot/refresh-token"
+export const PROFILE_STORAGE_NAME = "auto/spot/profile"
