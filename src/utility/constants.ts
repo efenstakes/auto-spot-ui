a7c186ee24da5f7de37f5523994daecf9bf272c5
Efen Stakes <efenstakes101@gmail.com>
2023-08-20 08:20:33 +0300

    add app routes and data

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,7 +1,115 @@
 
 
-export const SERVER_URL = ""
+export const SERVER_URL = "http://localhost:1234"
 
 export const PROFILE_ACCESS_TOKEN = "auto/spot/access-token"
 export const PROFILE_REFRESH_TOKEN = "auto/spot/refresh-token"
 export const PROFILE_STORAGE_NAME = "auto/spot/profile"
+
+
+
+// routes
+export const Routes = {
+    home: "/",
+    cart: "/cart",
+    myProfile: "/profile/my",
+    myOrders: "/orders/my",
+}
+
+
+export const CAR_BRANDS = [
+    
+    "Toyota",
+    "Honda",
+    "Nissan",
+    "Subaru",
+    "Audi",
+    "Mazda",
+    "Suzuki",
+
+    "Volkswagen",
+    "Mercedes-Benz",
+    "BMW",
+    
+    "Kia",
+    "Hyundai",
+    "Jeep",
+    "Lexus",
+    "Chevrolet",
+    "Ford",
+    "Volvo",
+    "Porsche",
+    "Lamborghini",
+    "Jaguar",
+    "Land Rover",
+    "Chrysler",
+    "Cadillac",
+    "Acura",
+    "Infiniti",
+    "Mitsubishi",
+
+]
+
+export const BRAND_MODELS = [
+    {
+      brand: "Toyota",
+      models: ["Corolla", "Camry", "RAV4", "Highlander", "Tacoma"]
+    },
+    {
+      brand: "Honda",
+      models: ["Civic", "Accord", "CR-V", "Pilot", "Fit"]
+    },
+    {
+      brand: "Ford",
+      models: ["F-150", "Focus", "Mustang", "Escape", "Explorer"]
+    },
+    {
+      brand: "Chevrolet",
+      models: ["Silverado", "Cruze", "Malibu", "Equinox", "Traverse"]
+    },
+    {
+      brand: "Nissan",
+      models: ["Altima", "Maxima", "Rogue", "Murano", "Sentra"]
+    },
+    {
+      brand: "Volkswagen",
+      models: ["Golf", "Jetta", "Passat", "Tiguan", "Atlas"]
+    },
+    {
+      brand: "BMW",
+      models: ["3 Series", "5 Series", "X3", "X5", "7 Series"]
+    },
+    {
+      brand: "Mercedes-Benz",
+      models: ["C-Class", "E-Class", "S-Class", "GLC", "GLE"]
+    },
+    {
+      brand: "Audi",
+      models: ["A3", "A4", "A6", "Q3", "Q5"]
+    },
+    {
+      brand: "Kia",
+      models: ["Forte", "Optima", "Sorento", "Sportage", "Telluride"]
+    },
+    {
+      brand: "Hyundai",
+      models: ["Elantra", "Sonata", "Tucson", "Santa Fe", "Kona"]
+    },
+    {
+      brand: "Mazda",
+      models: ["Mazda3", "Mazda6", "CX-5", "CX-9", "MX-5 Miata"]
+    },
+    {
+      brand: "Subaru",
+      models: ["Impreza", "Outback", "Forester", "Crosstrek", "Legacy"]
+    },
+    {
+      brand: "Jeep",
+      models: ["Wrangler", "Cherokee", "Grand Cherokee", "Renegade", "Compass"]
+    },
+    {
+      brand: "Lexus",
+      models: ["ES", "GS", "RX", "NX", "LS"]
+    },
+];
+  
