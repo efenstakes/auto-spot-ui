29772a3a51ae438f5434f12006321b6172e2a107
Efen Stakes <efenstakes101@gmail.com>
2023-12-11 18:43:05 +0300

    add form data - car models and brands

---
+++ src/utility/form_data.ts
@@ -0,0 +1,64 @@
+
+
+export const CarBrandsAndModels = [
+  {
+    brand: "Toyota",
+    models: ["Corolla", "Camry", "RAV4", "Highlander", "Tacoma"]
+  },
+  {
+    brand: "Honda",
+    models: ["Civic", "Accord", "CR-V", "Pilot", "Fit"]
+  },
+  {
+    brand: "Ford",
+    models: ["F-150", "Focus", "Mustang", "Escape", "Explorer"]
+  },
+  {
+    brand: "Chevrolet",
+    models: ["Silverado", "Cruze", "Malibu", "Equinox", "Traverse"]
+  },
+  {
+    brand: "Nissan",
+    models: ["Altima", "Maxima", "Rogue", "Murano", "Sentra"]
+  },
+  {
+    brand: "Volkswagen",
+    models: ["Golf", "Jetta", "Passat", "Tiguan", "Atlas"]
+  },
+  {
+    brand: "BMW",
+    models: ["3 Series", "5 Series", "X3", "X5", "7 Series"]
+  },
+  {
+    brand: "Mercedes-Benz",
+    models: ["C-Class", "E-Class", "S-Class", "GLC", "GLE"]
+  },
+  {
+    brand: "Audi",
+    models: ["A3", "A4", "A6", "Q3", "Q5"]
+  },
+  {
+    brand: "Kia",
+    models: ["Forte", "Optima", "Sorento", "Sportage", "Telluride"]
+  },
+  {
+    brand: "Hyundai",
+    models: ["Elantra", "Sonata", "Tucson", "Santa Fe", "Kona"]
+  },
+  {
+    brand: "Mazda",
+    models: ["Mazda3", "Mazda6", "CX-5", "CX-9", "MX-5 Miata"]
+  },
+  {
+    brand: "Subaru",
+    models: ["Impreza", "Outback", "Forester", "Crosstrek", "Legacy"]
+  },
+  {
+    brand: "Jeep",
+    models: ["Wrangler", "Cherokee", "Grand Cherokee", "Renegade", "Compass"]
+  },
+  {
+    brand: "Lexus",
+    models: ["ES", "GS", "RX", "NX", "LS"]
+  },
+]
