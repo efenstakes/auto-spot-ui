b7686ddcc4049fface2c519514681888d7851074
Efen Stakes <efenstakes101@gmail.com>
2023-12-11 18:47:52 +0300

    update cart item model

--- src/models/cart.ts
+++ src/models/cart.ts
@@ -1,8 +1,18 @@
-import IProduct from "./product";
-
+import IProduct, { IProductVariant } from "./product";
 
 
 export default interface ICartItem {
-    product: IProduct
-    quantity: number
+    // product: IProduct
+
+    _id?: string
+    name?: string
+    category?: string
+    brand?: string
+    model?: string
+    year?: number
+
+    price?: number
+
+    quantity?: number
+    variant?: IProductVariant
 }
