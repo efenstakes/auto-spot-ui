6ee389baf62c2f13c1c48fada05b4def704b5359
Efen Stakes <efenstakes101@gmail.com>
2023-12-17 16:40:49 +0300

    add mobile state and drawer

--- src/app/shop/filters.tsx
+++ src/app/shop/filters.tsx
@@ -3,10 +3,12 @@ import { ISelectedFilters } from '@/models/filters'
 import { containerVariants, itemFadeInVariants, itemVariants } from '@/styles/variants'
 import { CAR_BRANDS } from '@/utility/constants'
 import { CarBrandsAndModels } from '@/utility/form_data'
-import { Chip, Fab } from '@mui/material'
+import { Chip, Drawer, Fab } from '@mui/material'
+import { useWindowWidth } from '@react-hook/window-size'
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useState } from 'react'
 import { BsSendFill } from 'react-icons/bs'
+import DrawerFilters from './drawer_filters'
 
 
 type Props = {
@@ -17,13 +19,20 @@ type Props = {
     models: Array<string>
 }
 const Filters = ({  onClickYear, onClickBrand, onClickModel, filters, models, }: Props) => {
+    const width = useWindowWidth()
+    const isMobile = width < 840
+
+    const [showDrawer, setShowDrawer] = useState(false)
 
     // const models = CarBrandsAndModels.find(({ brand })=> filters?.brand == brand)?.models || []
-    return (
-        <>
-        
-            {/* title */}
-            <motion.div
+    
+    if( isMobile ) {
+
+        return (
+            <>
+                
+                {/* title */}
+                <motion.div
                     className='row ca_end ma_space_btn'
                     variants={containerVariants}
                     initial="initial"
@@ -37,84 +46,142 @@ const Filters = ({  onClickYear, onClickBrand, onClickModel, filters, models, }:
 
                     {/* contact us */}
                     <motion.div variants={itemVariants}>
-                    {/* href='mailto:autospot254@gmail.com' */}
-                        {/* <Link> */}
-                            <Fab
-                                size='small'
-                                color="primary"
-                                aria-label="inquire"
-                                variant='extended'
-                                style={{
-                                    padding: '1rem',
-                                    textTransform: 'none',
-                                    boxShadow: 'none',
-                                }}
-                            >
-                                Inquire
-                                <BsSendFill style={{ marginLeft: '.5rem', fontSize: '.8rem', }} />
-                            </Fab>
-                        {/* </Link> */}
+                        <Fab
+                            size='small'
+                            color="primary"
+                            aria-label="inquire"
+                            variant='extended'
+                            style={{
+                                padding: '1rem',
+                                textTransform: 'none',
+                                boxShadow: 'none',
+                            }}
+                            onClick={()=> setShowDrawer(true)}
+                        >
+                            Filter
+                        </Fab>
                     </motion.div>
 
                 </motion.div>
                 <VSpacer space={2} />
 
-                {/* brand filters */}
-                <motion.div variants={containerVariants} initial="initial" animate="animate" className='row_wrapped ca_center model_chips_container'>
-                    {
-                    [ "All", ...CAR_BRANDS ].map((brand)=> {
-
-                        return (
-                        <motion.div key={brand} variants={itemFadeInVariants} onClick={()=> onClickBrand(brand)}>
-                            <Chip label={brand} color='primary' variant={ filters.brand == brand ? "filled" : "outlined" } />
-                        </motion.div>
-                        )
-                    })
-                    }
-                </motion.div>
-                <VSpacer space={2} />
-
-                {/* models and years */}
                 {
-                    filters.brand && filters.brand != "All" &&
-                    <div className='row ma_end ca_center' style={{ gap: '1rem', }}>
-
-                        <select
-                            className='select'
-                            value={filters?.model}
-                            onChange={
-                                (e)=> onClickModel(e.target?.value)
-                            }
+                    showDrawer &&
+                        <Drawer
+                            anchor={'bottom'}
+                            open={showDrawer}
+                            onClose={()=> setShowDrawer(false)}
                         >
-                            {
-                                [ "All", ...models, ].map((brand)=> {
+                            <DrawerFilters
+                                close={() => setShowDrawer(false)}
+                                onClickYear={onClickYear}
+                                onClickBrand={onClickBrand}
+                                onClickModel={onClickModel}
+                                filters={filters}
+                                models={models}
+                            />
+                        </Drawer>
+                }
 
-                                    return (
-                                    <option key={brand} value={brand}>{brand}</option>
-                                    )
-                                })
-                            }
-                        </select>
+            </>
+        )
+    }
+    return (
+        <>
+        
+            {/* title */}
+            <motion.div
+                className='row ca_end ma_space_btn'
+                variants={containerVariants}
+                initial="initial"
+                animate="animate"
+            >
 
-                        <select
-                            className='select'
-                            value={filters?.year}
-                            onChange={
-                                (e)=> onClickYear(e.target?.value)
-                            }
+                {/* title */}
+                <motion.p className='title_7' variants={itemVariants}>
+                    Shop
+                </motion.p>
+
+                {/* contact us */}
+                <motion.div variants={itemVariants}>
+                {/* href='mailto:autospot254@gmail.com' */}
+                    {/* <Link> */}
+                        <Fab
+                            size='small'
+                            color="primary"
+                            aria-label="inquire"
+                            variant='extended'
+                            style={{
+                                padding: '1rem',
+                                textTransform: 'none',
+                                boxShadow: 'none',
+                            }}
                         >
+                            Inquire
+                            <BsSendFill style={{ marginLeft: '.5rem', fontSize: '.8rem', }} />
+                        </Fab>
+                    {/* </Link> */}
+                </motion.div>
+
+            </motion.div>
+            <VSpacer space={2} />
+
+            {/* brand filters */}
+            <motion.div variants={containerVariants} initial="initial" animate="animate" className='row_wrapped ca_center model_chips_container'>
+                {
+                [ "All", ...CAR_BRANDS ].map((brand)=> {
+
+                    return (
+                    <motion.div key={brand} variants={itemFadeInVariants} onClick={()=> onClickBrand(brand)}>
+                        <Chip label={brand} color='primary' variant={ filters.brand == brand ? "filled" : "outlined" } />
+                    </motion.div>
+                    )
+                })
+                }
+            </motion.div>
+            <VSpacer space={2} />
+
+            {/* models and years */}
+            {
+                filters.brand && filters.brand != "All" &&
+                <div className='row ma_end ca_center' style={{ gap: '1rem', }}>
+
+                    <select
+                        className='select'
+                        value={filters?.model}
+                        onChange={
+                            (e)=> onClickModel(e.target?.value)
+                        }
+                    >
                         {
-                            Array.from({ length: 15 }, (_, i)=> (new Date()).getFullYear() - i).map((yr)=> {
+                            [ "All", ...models, ].map((brand)=> {
 
-                                return <option key={yr} value={yr}>{yr}</option>
+                                return (
+                                <option key={brand} value={brand}>{brand}</option>
+                                )
                             })
                         }
-                        </select>
+                    </select>
 
-                    </div>
-                }
-                <VSpacer space={2} />
-                
+                    <select
+                        className='select'
+                        value={filters?.year}
+                        onChange={
+                            (e)=> onClickYear(e.target?.value)
+                        }
+                    >
+                    {
+                        Array.from({ length: 15 }, (_, i)=> (new Date()).getFullYear() - i).map((yr)=> {
+
+                            return <option key={yr} value={yr}>{yr}</option>
+                        })
+                    }
+                    </select>
+
+                </div>
+            }
+            <VSpacer space={2} />
+            
         </>
     )
 }
