24e86bf4ae846ec5fcdb774eafdd1a31f5a6c797
Efen Stakes <efenstakes101@gmail.com>
2023-12-17 16:41:35 +0300

    add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Appbar from '@/components/appbar/component'
 import Experience from '@/components/experience/component'
 import VSpacer from '@/components/v_spacer/component'
 import Shop from './shop/shop'
+import AboutProducts from '@/components/about_products/component'
+import AvailableProducts from '@/components/available_products/component'
 
 
 export default function Home() {
@@ -18,6 +20,13 @@ export default function Home() {
       {/* experience */}
       <VSpacer space={8} />
       <Experience />
+      <VSpacer space={4} />
+
+      {/* what we do */}
+      <AboutProducts />
+      <VSpacer space={4} />
+
+      <AvailableProducts />
       <VSpacer space={4} />
 
       <Shop />
